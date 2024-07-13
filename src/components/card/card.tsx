421d839fa03ba3d380b1042027617b2423714b86
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-07-13 14:41:56 +0400

    refactor: rewrite card to functional component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import truncateString from '../../utils/truncateString';
 import './card.scss';
 
@@ -9,28 +9,31 @@ interface CardProps {
   className?: string;
 }
 
-export default class Card extends Component<CardProps> {
-  render() {
-    const { title, description, image, className } = this.props;
+const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  image,
+  className
+}) => {
+  const showDescription = description
+    ? truncateString(description, 150)
+    : 'No description';
+  const cardImage = image
+    ? image
+    : 'https://www.svgrepo.com/show/508699/landscape-placeholder.svg';
 
-    const showDescription = description
-      ? truncateString(description, 150)
-      : 'No description';
-    const cardImage = image
-      ? image
-      : 'https://www.svgrepo.com/show/508699/landscape-placeholder.svg';
-
-    return (
-      <div className={`card ${className}`}>
-        <img src={cardImage} alt="Show poster" className="card-image" />
-        <div className="card-content">
-          <h2 className="card-title">{title}</h2>
-          <p
-            className="card-description"
-            dangerouslySetInnerHTML={{ __html: showDescription }}
-          ></p>
-        </div>
+  return (
+    <div className={`card ${className}`}>
+      <img src={cardImage} alt="Show poster" className="card-image" />
+      <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p
+          className="card-description"
+          dangerouslySetInnerHTML={{ __html: showDescription }}
+        ></p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Card;
