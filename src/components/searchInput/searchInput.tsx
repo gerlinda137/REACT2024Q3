75ebef3b92775d8dab1fc1f8a2a4b406409f5128
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2024-07-13 19:39:04 +0400

    refactor: searchInput wip

--- src/components/searchInput/searchInput.tsx
+++ src/components/searchInput/searchInput.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Component } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { SearchResult } from '../../api/apiHandler';
 import './searchInput.scss';
 
@@ -8,58 +8,96 @@ interface SearchProps {
   handleSearch: (query: string) => void;
 }
 
-interface SearchState {
-  query: string;
-  results: SearchResult[];
-}
+const SearchInput: React.FC<SearchProps> = ({
+  className,
+  placeholder,
+  handleSearch
+}) => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<SearchResult[]>([]);
 
-export default class SearchInput extends Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = {
-      query: '',
-      results: []
-    };
-
-    this.handleSearchChange = this.handleSearchChange.bind(this);
-    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
-  }
-
-  public setResults(results: SearchResult[]) {
-    this.setState({ results });
-  }
-
-  handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ query: event.target.value });
-  }
-
-  handleSearchSubmit() {
-    const { query } = this.state;
-    this.props.handleSearch(query);
-    localStorage.setItem('query', query);
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const prevQuery = localStorage.getItem('query');
     if (prevQuery) {
-      this.setState({ query: prevQuery });
+      setQuery(prevQuery);
     }
-  }
+  }, []);
 
-  render() {
-    const { className, placeholder } = this.props;
-    const { query } = this.state;
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
 
-    return (
-      <section className={className}>
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={query}
-          onChange={this.handleSearchChange}
-        />
-        <button onClick={this.handleSearchSubmit}>Search</button>
-      </section>
-    );
-  }
-}
+  const handleSearchSubmit = () => {
+    handleSearch(query);
+    localStorage.setItem('query', query);
+  };
+
+  const setSearchResults = (results: SearchResult[]) => {
+    setResults(results);
+  };
+
+  return (
+    <section className={className}>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onChange={handleSearchChange}
+      />
+      <button onClick={handleSearchSubmit}>Search</button>
+    </section>
+  );
+};
+
+// export default class SearchInput extends Component<SearchProps, SearchState> {
+//   constructor(props: SearchProps) {
+//     super(props);
+//     this.state = {
+//       query: '',
+//       results: []
+//     };
+
+//     this.handleSearchChange = this.handleSearchChange.bind(this);
+//     this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+//   }
+
+//   public setResults(results: SearchResult[]) {
+//     this.setState({ results });
+//   }
+
+//   handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+//     this.setState({ query: event.target.value });
+//   }
+
+//   handleSearchSubmit() {
+//     const { query } = this.state;
+//     this.props.handleSearch(query);
+//     localStorage.setItem('query', query);
+//   }
+
+//   componentDidMount() {
+//     const prevQuery = localStorage.getItem('query');
+//     if (prevQuery) {
+//       this.setState({ query: prevQuery });
+//     }
+//   }
+
+//   render() {
+//     const { className, placeholder } = this.props;
+//     const { query } = this.state;
+
+//     return (
+//       <section className={className}>
+//         <input
+//           type="text"
+//           placeholder={placeholder}
+//           value={query}
+//           onChange={this.handleSearchChange}
+//         />
+//         <button onClick={this.handleSearchSubmit}>Search</button>
+//       </section>
+//     );
+//   }
+// }
+
+export default SearchInput;
